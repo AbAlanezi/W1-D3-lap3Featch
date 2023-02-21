f057fc32501068254e11b9138f5fcd4f00b58705
Abdalazez <abalanazi@nadeer.com.sa>
2023-02-21 15:15:04 +0300

    lap3

---
+++ dist/app.js
@@ -0,0 +1,106 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// const express = require('express')
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+let data = require('./data.json');
+app.get('/', (req, res) => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => res.json(json));
+    // res.send(json)
+});
+app.delete('/:id', (req, res) => {
+    const Id = req.params.id;
+    delete data[Id];
+    res.json(data);
+});
+app.post('/', (req, res) => {
+    data.push({ "id": 6, "name": "khaled", "age": 30 });
+    res.json(data);
+});
+app.put('/:id', (req, res) => {
+    const Id = req.params.id;
+    data.filter((e) => e.id == Id ? e.id = 1 : e.id);
+    res.json(data);
+});
+//lap2 
+// const fun1 = (req, res, next)=>{
+//     res.json({"masseg":"fun"})
+//     next();
+// }
+// const fun2 = (req, res)=>{
+//     console.log('gfgffgf');
+//     res.json({"masseg":"fun1"})
+// }
+// app.get('/',fun1,fun2);
+// app.get('/user/:name',(req, res) =>{
+//     res.send("heloo" + req.params.name)
+// });
+// app.use((req, res, next) => {
+//     console.log('is runing')
+//     next()
+// })
+// app.get("/",(req, res)=>{
+//     if(req.query.Admin == "true"){
+//         res.send("good")
+//     }else{
+//         res.send("you are not admin")
+//     }
+// })
+// app.get('/add/:num1/:num2',(req, res) =>{
+//    let Num1 = req.params.num1
+//    let Num2 = req.params.num2
+//    let coNum1 = parseInt(Num1)
+//    let coNum2 = parseInt(Num2)
+//    let add = coNum1 + coNum2
+//    if(req.query.Admin == "true"){
+//        res.send(""+add)
+//    }else{
+//     res.send('you are not admin')
+//    }
+// });
+// app.get('/sub/:num1/:num2',(req, res) =>{
+//     let Num1 = req.params.num1
+//     let Num2 = req.params.num2
+//    let coNum1 = parseInt(Num1)
+//    let coNum2 = parseInt(Num2)
+//     let add = coNum1 - coNum2
+//     res.send(""+add)
+// });
+// app.get('/mul/:num1/:num2',(req, res) =>{
+//     let Num1 = req.params.num1
+//     let Num2 = req.params.num2
+//    let coNum1 = parseInt(Num1)
+//    let coNum2 = parseInt(Num2)
+//     let add = coNum1 * coNum2
+//     res.send(""+add)
+// });
+// app.get('/div/:num1/:num2',(req, res) =>{
+//     let Num1 = req.params.num1
+//     let Num2 = req.params.num2
+//    let coNum1 = parseInt(Num1)
+//    let coNum2 = parseInt(Num2)
+//     let add = coNum1 / coNum2
+//     res.send(""+add)
+// });
+// app.get('/',(req, res)=>{
+// console.log("heloo");
+// res.json({"masseg":"heloo"})
+// // res.status(200).json({"masseg":"heloo"})
+// // res.end()
+// // res.send("is runing")
+// })
+// app.post('/user',(req, res) => {
+//     res.status(200).json({"masseg":"post"})
+// })
+// app.put('/user',(req, res) => {
+//     res.status(200).json({"masseg":"put"})
+// })
+// app.delete('/user',(req, res) => {
+//     res.status(200).json({"masseg":"delete"})
+// })
+app.listen(3008);
